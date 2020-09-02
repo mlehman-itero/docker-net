3d9bdefaef3a223a7bced45d47e9942a7046fa55
mlehman <mlehman@iterogroup.com>
2020-09-02 13:55:38 -0400

    adding some unit tests

---
+++ web.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using web.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+namespace web.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        private readonly WeatherForecastController _controller;
+
+        public WeatherForecastControllerTests()
+        {
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            _controller = new WeatherForecastController(loggerMock.Object);
+        }
+
+        [Fact]
+        public void Get_Should_Not_Be_Empty()
+        {
+            var result = _controller.Get();
+            Assert.NotEmpty(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(6)]
+        [InlineData(3)]
+        public void Get_Should_Not_Return_X_Forecasts(int value)
+        {
+            var result = _controller.Get();
+            // Assert.Equal(value, result.)
+        }
+    }
+}
